ee2d0d8269b90758e61fc39409976eaec22ceaf6
Stav Shamir <stav.shamir@ecitele.com>
2018-12-11 13:18:58 +0200

    Implement listening history list and item components

--- src/app/listening-history/listening-history-list/listening-history-item/listening-history-item.component.ts
+++ src/app/listening-history/listening-history-list/listening-history-item/listening-history-item.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ListeningHistoryItem } from '../../listening-history-item.model';
 
 @Component({
-  selector: 'app-listening-history-item',
+  selector: '[app-listening-history-item]',
   templateUrl: './listening-history-item.component.html',
   styleUrls: ['./listening-history-item.component.css']
 })
 export class ListeningHistoryItemComponent implements OnInit {
+
+  @Input() item: ListeningHistoryItem;
 
   constructor() { }
 
--- src/app/listening-history/listening-history-list/listening-history-list.component.ts
+++ src/app/listening-history/listening-history-list/listening-history-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ListeningHistoryItem } from '../listening-history-item.model';
 
 @Component({
   selector: 'app-listening-history-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listening-history-list.component.css']
 })
 export class ListeningHistoryListComponent implements OnInit {
+
+  listeningHistory: ListeningHistoryItem[] = [
+    new ListeningHistoryItem('Mirror Mirror', ['Blind Guardian'], 'Nightfall In Middle Earth', 'https://i.scdn.co/image/50661fbc143dd5e53f5812193cda31c2fdc6e10f', new Date()),
+    new ListeningHistoryItem('Bombshell', ['Chilly Gonzales', 'Jarvis Cocker'], 'Room 29', 'https://i.scdn.co/image/61b0f1f3d81323b2c86ba14bafac5d4e7a0d58e9', new Date())
+  ];
 
   constructor() { }
 
