8a6e52993c361f10ab4f83cdbcd2980575925289
Stav Shamir <stav.shamir@ecitele.com>
2018-12-12 14:08:06 +0200

    Improve listening history navigation component

--- src/app/listening-history/listening-history-navigation/listening-history-navigation.component.ts
+++ src/app/listening-history/listening-history-navigation/listening-history-navigation.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListeningHistoryNavigationComponent implements OnInit {
 
+  currentPage: number;
+  numberOfPages: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.currentPage = 1;
+    this.numberOfPages = 231;
   }
 
 }
